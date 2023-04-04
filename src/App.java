e5bbf0d447a71606c4a5723f7c3fa6d468c81a21
connor-eg <connoregarcia@gmail.com>
2023-04-04 10:03:48 -0500

    refactored

--- src/App.java
+++ src/App.java
@@ -1,11 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(calculateVotes(new VoteItem(13, 0)));
-        System.out.println(calculateVotes(new VoteItem(2, 33)));
-        System.out.println(calculateVotes(new VoteItem(132, 132)));
-    }
-
-    public static int calculateVotes(VoteItem item){
-        return item.upvotes - item.downvotes;
+        System.out.println(new VoteItem(13, 0).calculateVotes());
+        System.out.println(new VoteItem(2, 33).calculateVotes());
+        System.out.println(new VoteItem(132, 132).calculateVotes());
     }
 }
--- src/VoteItem.java
+++ src/VoteItem.java
@@ -1,7 +1,7 @@
 public class VoteItem {
     //It is okay for both of these to be public because they are final.
-    public final int upvotes;
-    public final int downvotes;
+    private final int upvotes;
+    private final int downvotes;
 
     public VoteItem(int upvotes, int downvotes) {
         if(upvotes < 0 || downvotes < 0){
@@ -9,5 +9,12 @@ public class VoteItem {
         }
         this.upvotes = upvotes;
         this.downvotes = downvotes;
+    }
+
+    public int getUpvotes() { return upvotes; }
+    public int getDownvotes() { return downvotes; }
+
+    public int calculateVotes(){
+        return upvotes - downvotes;
     }
 }
