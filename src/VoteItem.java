63b3ced2f9ef85f79c21ad589b286442dbb3dc4c
connor-eg <connoregarcia@gmail.com>
2023-04-04 10:06:17 -0500

    fixed comments

--- src/VoteItem.java
+++ src/VoteItem.java
@@ -1,8 +1,8 @@
 public class VoteItem {
-    //It is okay for both of these to be public because they are final.
     private final int upvotes;
     private final int downvotes;
 
+    //One thing of note - neither upvotes nor downvotes is allowed to be negative
     public VoteItem(int upvotes, int downvotes) {
         if(upvotes < 0 || downvotes < 0){
             throw new IllegalStateException("Upvotes and downvotes cannot be negative!");
